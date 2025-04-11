e7990b2afff1c845e837b5dbc2127bcc4d896e25
Brook Tekle (Opshadow) <126861167+Brooktek@users.noreply.github.com>
2025-04-11 22:34:21 +0300

    update

---
+++ app/src/main/java/com/example/temero/JavaActivity.java
@@ -0,0 +1,19 @@
+package com.example.temero;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java);
+
+    }
+}
--- app/src/main/java/com/example/temero/MainActivity.java
+++ app/src/main/java/com/example/temero/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.temero;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CardView card1 = findViewById(R.id.card1);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open a new Activity (e.g., Java Basics Activity)
+                Intent intent = new Intent(MainActivity.this, JavaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
